0492c514c4e118b7212cfc358c472ca2a6af60d7
Murat Kasimov <iokasimov.m@gmail.com>
2024-04-01 14:57:17 +0700

    Finish Arithmetizable instance for Offer

--- src/ZkFold/P2P/Payment.hs
+++ src/ZkFold/P2P/Payment.hs
@@ -2,23 +2,21 @@
 module ZkFold.P2P.Payment where
 
 import           Prelude                        hiding (Bool, Eq ((==)), (*), (+), length, splitAt)
+import           Control.Monad.State.Lazy        (evalState, state)
 
+import           ZkFold.Prelude
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Data.Vector
 import           ZkFold.Symbolic.Cardano.Types.Tx
+import           ZkFold.Symbolic.Cardano.Types.Address
 import           ZkFold.Symbolic.Compiler
 import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.Eq
 import           ZkFold.Symbolic.Data.UInt
 
-data CardanoPaymentCredentials a
-    = ShellyPaymentCredentials (Vector 56 (UInt 8 a))
-  -- TODO: | DaedalusPaymentCredentials (...)
-
--- TODO
-instance Arithmetizable a x => Arithmetizable a (CardanoPaymentCredentials x) where
 
 -- Should include part of PAN, account number holder, probably with PCI DSS masking
+-- Can be finished when arithmetizable ByteStrings be ready
 data FiatAccount a = FiatAccount a
 
 instance (Ring a, Finite a, MultiplicativeGroup a) => Eq (Bool a) (FiatAccount a)
@@ -39,7 +37,7 @@ data Offer a = Offer
     { oFiatAccount :: FiatAccount a
     , oFiatAmount :: UInt 64 a
     , oFiatCurrency :: ISO427 a
-    , oCardanoAddress :: CardanoPaymentCredentials a
+    , oCardanoAddress :: Address a
     , oCardanoAmount :: UInt 64 a
     }
 
@@ -51,6 +49,20 @@ instance (Arithmetizable a (UInt 64 x), Arithmetizable a x) => Arithmetizable a 
             <*> arithmetize fCurrency
             <*> arithmetize cAddress
             <*> arithmetize cAmount
+    restore offer =
+        if length offer == typeSize @a @(Offer x)
+        then flip evalState offer $ Offer
+            <$> do restore <$> do state . splitAt $ typeSize @a @(FiatAccount x)
+            <*> do restore <$> do state . splitAt $ typeSize @a @(UInt 64 x)
+            <*> do restore <$> do state . splitAt $ typeSize @a @(ISO427 x)
+            <*> do restore <$> do state . splitAt $ typeSize @a @(Address x)
+            <*> do restore <$> do state . splitAt $ typeSize @a @(UInt 64 x)
+        else error "restore Offer: wrong number of arguments"
+    typeSize = typeSize @a @(FiatAccount x)
+             + typeSize @a @(UInt 64 x)
+             + typeSize @a @(ISO427 x)
+             + typeSize @a @(Address x)
+             + typeSize @a @(UInt 64 x)
 
 zkSmartContract ::
     Eq (Bool a) (Offer a) =>
