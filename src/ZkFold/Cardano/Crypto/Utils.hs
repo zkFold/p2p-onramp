2f92d2e03d4e4d030cb117af7ad7635141e6a64f
ajuggler <ajuggler@users.noreply.github.com>
2025-08-22 20:51:34 -0600

    Added test module

--- src/ZkFold/Cardano/Crypto/Utils.hs
+++ src/ZkFold/Cardano/Crypto/Utils.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8  as B8
 import qualified Data.ByteString.Lazy   as BL
 import           Prelude                (Either (..), FilePath, IO, String,
-                                         pure, ($), (-), (<$>), (>=))
+                                         pure, (>>=), ($), (-), (<$>), (>=))
 
 
 data KeyFile = KeyFile { cborHex :: String }
@@ -17,26 +17,31 @@ data KeyFile = KeyFile { cborHex :: String }
 instance FromJSON KeyFile where
     parseJSON = withObject "KeyFile" $ \v -> KeyFile <$> v .: "cborHex"
 
--- | Extract the last 32 bytes of a key
+-- | Obtain the last 32 bytes of a key as string
+stringToKey :: String -> Either String B8.ByteString
+stringToKey hexStr = do
+  rawBytes <- B16.decode $ B8.pack hexStr
+  if B8.length rawBytes >= 32
+    then Right $ B8.drop (B8.length rawBytes - 32) rawBytes
+    else Left "Unable to retrieve rawbytes"
+  
+-- | Extract the last 32 bytes of a key in file
 extractKey :: FilePath -> IO (Either String B8.ByteString)
 extractKey filePath = do
   fileContent <- BL.readFile filePath
 
   pure $ do
     KeyFile hexStr <- eitherDecode fileContent
-    rawBytes       <- B16.decode $ B8.pack hexStr
+    stringToKey hexStr
 
-    if B8.length rawBytes >= 32
-      then Right $ B8.drop (B8.length rawBytes - 32) rawBytes
-      else Left "Unable to retrieve rawbytes"
+-- | Obtain secret key from bytes
+bytesToSecretKey :: B8.ByteString -> Either String SecretKey
+bytesToSecretKey skBytes = case secretKey skBytes of
+  CryptoPassed key -> Right key
+  CryptoFailed _   -> Left "Invalid skey"
 
--- | Extract secret key
+-- | Extract secret key in file
 extractSecretKey :: FilePath -> IO (Either String SecretKey)
 extractSecretKey filePath = do
   skBytesE <- extractKey filePath
-
-  pure $ do
-    skBytes <- skBytesE
-    case secretKey skBytes of
-      CryptoPassed key -> Right key
-      CryptoFailed _   -> Left "Invalid skey"
+  pure $ skBytesE >>= bytesToSecretKey
